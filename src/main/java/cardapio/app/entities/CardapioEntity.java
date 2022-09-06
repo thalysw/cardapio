b212f96714cf5d0913acbdbfd7b2091d867b5f00
thalysw <thalys.welson@gmail.com>
2022-09-06 00:06:35 -0300

    mapeamento da tabela e colunas, e configuracao do banco de dados

--- src/main/java/cardapio/app/entities/CardapioEntity.java
+++ src/main/java/cardapio/app/entities/CardapioEntity.java
@@ -1,9 +1,26 @@
 package cardapio.app.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL) // Ocultará os itens que estiverem nulos.
+@Table(name = "TB_CARDAPIO")
+@Entity
 public class CardapioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
+    @Column(name = "NM_TITULO")
     String titulo;
+    @Column(name = "NM_DESCRICAO")
     String descricao;
+    @Column(name = "VL_VALOR")
     Double valor;
+    @Column(name = "VL_QUANTIDADE")
     Long quantidade;
+
 }
